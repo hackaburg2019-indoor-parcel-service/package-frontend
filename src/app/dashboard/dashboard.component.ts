a9f821ecc2bbcb6dbf1bc459d17dca9ed5053340
Yleisnero <fischi12325@gmail.com>
2019-05-25 00:36:37 +0200

    stand für tobi

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
-  title = 'Titel';
+  title = 'indoor-parcel-service';
   constructor() { }
 
   ngOnInit() {
--- src/app/pickup/pick-up-parcel.component.ts
+++ src/app/pickup/pick-up-parcel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-pick-up-parcel',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickUpParcelComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+
   }
 
 }
